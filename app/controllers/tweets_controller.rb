538d66ed72cc77f5396d1b64aee8533454e355df
EC2 Default User <ec2-user@ip-172-31-24-34.us-east-2.compute.internal>
2019-03-27 11:18:05 +0000

    first gitpractice

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,22 @@
+class TweetsController < ApplicationController
+#   def index
+#       @tweets = Tweet.all
+#   end
+    def index
+       @tweets = Tweet.all 
+    end
+    
+    def new
+    end
+    
+    def create
+    binding.pry
+    Tweet.create(tweet_params)
+    end
+
+    private
+    def tweet_params
+    params.permit(:text)
+    end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+    
+get 'tweets' => 'tweets#index'
+
+get 'tweets/new' => 'tweets#new'
+
+post 'tweets' => 'tweets#create'
+
+end
